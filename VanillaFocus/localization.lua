d700ed949253bb4740d81d94c6d498e62d9afeaf
Chris Schalago <chrisschalago@gmail.com>
2017-01-09 18:30:27 -0500

    Add enUS conditional

--- VanillaFocus/localization.lua
+++ VanillaFocus/localization.lua
@@ -1,5 +1,7 @@
 VANILLAFOCUS_VERSION = "Vanilla Focus v1.0"
+VANILLAFOCUS_LOCALE = GetLocale();
 
+if (VANILLAFOCUS_LOCALE == "enUS") then
 -- EN
 VANILLAFOCUS_LOADED = "Loaded - /vf"
 VANILLAFOCUS_HELP1 = " - Usage:"
@@ -8,4 +10,4 @@ VANILLAFOCUS_HELP3 = "Sets <name> as focus"
 VANILLAFOCUS_HELP4 = "Clears current focus"
 VANILLAFOCUS_HELP5 = "Casts <spell> on focus"
 VANILLAFOCUS_HELP6 = "Targets focus"
-
+end
