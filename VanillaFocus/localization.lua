efcec3f354448038049aeb66f522864b4faa8543
Chris Schalago <chrisschalago@gmail.com>
2017-01-09 18:58:20 -0500

    Update localization

--- VanillaFocus/localization.lua
+++ VanillaFocus/localization.lua
@@ -6,8 +6,8 @@ if (VANILLAFOCUS_LOCALE == "enUS") then
 VANILLAFOCUS_LOADED = "Loaded - /vf"
 VANILLAFOCUS_HELP1 = " - Usage:"
 VANILLAFOCUS_HELP2 = "Sets current target as focus"
-VANILLAFOCUS_HELP3 = "Sets <name> as focus"
+VANILLAFOCUS_HELP3 = "Sets NAME as focus"
 VANILLAFOCUS_HELP4 = "Clears current focus"
-VANILLAFOCUS_HELP5 = "Casts <spell> on focus"
+VANILLAFOCUS_HELP5 = "Casts SPELL on focus"
 VANILLAFOCUS_HELP6 = "Targets focus"
 end
