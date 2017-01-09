3abd00776b88dad6ab63738600edaf6ad1c5561d
Chris Schalago <chrisschalago@gmail.com>
2017-01-09 18:00:22 -0500

    Add OnLoad() and some other updates

--- VanillaFocus/VanillaFocus.lua
+++ VanillaFocus/VanillaFocus.lua
@@ -1,3 +1,35 @@
+
+
+
+function VanillaFocus_OnLoad()
+	SLASH_FOCUS1 = "/focus";
+	SLASH_CLEARFOCUS1 = "/clearfocus";
+	SLASH_FOCUSCAST1 = "/focuscast";
+	SLASH_TARGETFOCUS1 = "/targetfocus";
+	
+	SLASH_VANILLAFOCUS1 = "/vanillafocus";
+	SLASH_VANILLAFOCUS2 = "/vf";
+	SlashCmdList["VANILLAFOCUS"] = function(msg)
+		VanillaFocus_SlashCommandHandler(msg);
+	end
+	
+	DEFAULT_CHAT_FRAME:AddMessage(VANILLAFOCUS_VERSION..VANILLAFOCUS_LOADED);
+	
+	VanillaFocus_Focus = nil;
+end
+
+function VanillaFocus_SetFocus()
+end
+
+function VanillaFocus_ClearFocus()
+end
+
+function VanillaFocus_TargetFocus()
+end
+
+function VanillaFocus_FocusCast()
+end
+
 function VanillaFocus_SlashCommandHandler(msg) 
 	if (msg) then
 		local command = string.lower(msg);
@@ -8,9 +40,9 @@ end
 function VanillaFocus_Help()
 	DEFAULT_CHAT_FRAME:AddMessage(VANILLAFOCUS_VERSION..VANILLAFOCUS_HELP1);
 	DEFAULT_CHAT_FRAME:AddMessage("/focus         -- "..VANILLAFOCUS_HELP2);
-	DEFAULT_CHAT_FRAME:AddMessage("/focus NAME         -- "..VANILLAFOCUS_HELP3);
+	DEFAULT_CHAT_FRAME:AddMessage("/focus name         -- "..VANILLAFOCUS_HELP3);
 	DEFAULT_CHAT_FRAME:AddMessage("/clearfocus         -- "..VANILLAFOCUS_HELP4);
-	DEFAULT_CHAT_FRAME:AddMessage("/focuscast SPELLID -- "..VANILLAFOCUS_HELP5);
+	DEFAULT_CHAT_FRAME:AddMessage("/focuscast spell -- "..VANILLAFOCUS_HELP5);
 	DEFAULT_CHAT_FRAME:AddMessage("/targetfocus         -- "..VANILLAFOCUS_HELP6);
 end
 
--- VanillaFocus/localization.lua
+++ VanillaFocus/localization.lua
@@ -1,11 +1,11 @@
 VANILLAFOCUS_VERSION = "Vanilla Focus v1.0"
 
 -- EN
-VANILLAFOCUS_LOADED = "Loaded - /vanillafocus"
+VANILLAFOCUS_LOADED = "Loaded - /vf"
 VANILLAFOCUS_HELP1 = " - Usage:"
 VANILLAFOCUS_HELP2 = "Sets current target as focus"
-VANILLAFOCUS_HELP3 = "Sets NAME as focus"
+VANILLAFOCUS_HELP3 = "Sets <name> as focus"
 VANILLAFOCUS_HELP4 = "Clears current focus"
-VANILLAFOCUS_HELP5 = "Casts SPELLID on focus"
+VANILLAFOCUS_HELP5 = "Casts <spell> on focus"
 VANILLAFOCUS_HELP6 = "Targets focus"
 
