0df7594c495a8d76491d54fc3f6b7ac65f8cd0e9
Chris Schalago <chrisschalago@gmail.com>
2017-01-12 15:54:58 -0500

    Simplified function using TargetLastTarget()

--- VanillaFocus/VanillaFocus.lua
+++ VanillaFocus/VanillaFocus.lua
@@ -1,8 +1,5 @@
-local VANILLAFOCUS, namespace = ...
-local L = namespace.L
-
-VanillaFocus_Focus = nil;
 VANILLAFOCUS = {};
+VanillaFocus_Focus = nil;
 
 function VanillaFocus_OnLoad()
 	SLASH_VANILLAFOCUS1 = "/vanillafocus";
@@ -28,15 +25,15 @@ function VanillaFocus_OnLoad()
 		VanillaFocus_TargetFocus();
 	end
 	
-	DEFAULT_CHAT_FRAME:AddMessage("Vanilla Focus v1.0".."Loaded - /vf");
+	DEFAULT_CHAT_FRAME:AddMessage("Vanilla Focus v1.0"..": Loaded - /vf");
 end
 
 VanillaFocus_OnLoad();
 
 function VanillaFocus_SetFocus(name)
-	if (name ~= "") then
+	if  (name ~= "") then
 		VanillaFocus_Focus = name;
-	elseif (GetUnitName("target") ~= nill) then
+	elseif GetUnitName("target") ~= nil then
 		VanillaFocus_Focus = GetUnitName("target");
 	end
 end
@@ -46,33 +43,32 @@ function VanillaFocus_ClearFocus()
 end
 
 function VanillaFocus_TargetFocus()
-	if (VanillaFocus_Focus ~= nill) then
+	if  (VanillaFocus_Focus ~= nil) then
 		SlashCmdList["TARGET"](VanillaFocus_Focus);
 	end
 end
 
 function VanillaFocus_FocusCast(spell)
-	if (VanillaFocus_Focus ~= nill) then
-		VanillaFocus_currentTarget = GetUnitName("target");
+	if 	(VanillaFocus_Focus ~= nil) then
 		SlashCmdList["TARGET"](VanillaFocus_Focus);
 		CastSpellByName(spell);
-		TargetUnit(VanillaFocus_currentTarget);
+		TargetLastTarget(); 
 	end
 end
 
 function VanillaFocus_SlashCommandHandler(msg) 
-	if (msg) then
+	if  (msg) then
 		local command = string.lower(msg);
 		VanillaFocus_Help();
 	end
 end
 
 function VanillaFocus_Help()
-	DEFAULT_CHAT_FRAME:AddMessage(L["Vanilla Focus v1.0"]..L[" - Usage:"]);
-	DEFAULT_CHAT_FRAME:AddMessage("/focus         -- "..L[]Sets current target as focus"]);
-	DEFAULT_CHAT_FRAME:AddMessage("/focus name         -- "..L["Sets NAME as focus"]);
-	DEFAULT_CHAT_FRAME:AddMessage("/clearfocus         -- "..L["Clears current focus"]);
-	DEFAULT_CHAT_FRAME:AddMessage("/focuscast spell -- "..L["Casts SPELL on focus"]);
-	DEFAULT_CHAT_FRAME:AddMessage("/targetfocus         -- "..L["Targets focus"]);
+	DEFAULT_CHAT_FRAME:AddMessage("Vanilla Focus v1.0".." - Usage:");
+	DEFAULT_CHAT_FRAME:AddMessage("/focus         -- ".."Sets current target as focus");
+	DEFAULT_CHAT_FRAME:AddMessage("/focus name         -- ".."Sets NAME as focus");
+	DEFAULT_CHAT_FRAME:AddMessage("/clearfocus         -- ".."Clears current focus");
+	DEFAULT_CHAT_FRAME:AddMessage("/focuscast spell -- ".."Casts SPELL on focus");
+	DEFAULT_CHAT_FRAME:AddMessage("/targetfocus         -- ".."Targets focus");
 end
 
